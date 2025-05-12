e2b5d0466448ebd3e239171cbf5ce33c1f945327
tahmid-rahman <artahmid123@gmail.com>
2025-05-12 20:35:43 +0600

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,14 @@ import React from "react";
 import "../src/styles/globals.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import { AuthProvider } from "./contexts/AuthContext";
+// import { AuthProvider } from "./contexts/AuthContext";s
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/signup" element={<Login />} /> */}
-
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
